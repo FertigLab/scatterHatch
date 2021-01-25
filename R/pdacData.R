ca6e4573cb806f74729a7722b92a2e1cd2e29aa9
roofTiles <10423046+roofTiles@users.noreply.github.com>
2021-01-24 20:54:00 -0500

    meeting r cmd check
    Former-commit-id: 2e52fe56e1be49bf676097492bd0d420b5565af2

---
+++ R/pdacData.R
@@ -0,0 +1,19 @@
+#' Pancreatic Dual Adenocarcionma (PDAC) tissue-CyCIF dataset
+#'
+#' Single-cell resolution of 21 proteins and 3 cellular compartment in a human
+#' pancreatic dual adenocarcionma sample that spans across three distinct tissues:
+#' the pancreas, small intestine, and tumor.  160,000 cells profiled.
+#'
+#' @docType data
+#'
+#' @usage data(pdacData)
+#'
+#'
+#' @keywords datasets
+#'
+#' @references Lin et al. (2018) eLife 2018;7:e31657
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/29993362/}{PubMed})
+#'
+#' @source \href{http://spatial.rc.fas.harvard.edu/spatialgiotto/giotto.dataset.html}{Giotto Datasets}
+#'
+"pdacData"
