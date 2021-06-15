37ec335b912c832426994c36d3795c53df36bd58
roofTiles <guhatejas@gmail.com>
2021-06-14 20:09:06 -0400

    R CMD check update 1
    2 warnings, 1 note
    (licensing, qpdf, package size)

---
+++ R/pdacData.R
@@ -0,0 +1,8 @@
+#' Tissue-CyCIF data of Pancreatic Ductal Adenocarcinoma (PDAC)
+#'
+#' A dataset containing antibody expression of a human tissue sample with
+#' PDAC at a single-cell resolution used in Lin et al.
+#'
+#' @format A data frame with 158159 rows and 41 variables:
+#' @source \url{http://spatialgiotto.rc.fas.harvard.edu/giotto.cycif.html}
+"pdacData"
--- R/scatterHatch.R
+++ R/scatterHatch.R
@@ -19,10 +19,13 @@
 #' @param colorPalette Colors to be used for each group.  Default is color-blind friendly.
 #' @return ggplot2 object of scatterplot with hatched patterns for each group.
 #' @export
+#' @importFrom grDevices dev.size
+#' @importFrom stats median
 
 scatterHatch <- function(data, x, y, factor, legendTitle = "", pointSize = 1, pointAlpha = 0.5, gridSize = NULL, sparsePoints = NULL,
                          patternList = NULL, colorPalette = NULL){
 
+  ids <- xEnd <- xStart <- yEnd <- yStart <- NULL
   if (!(x %in% names(data))){ stop("x column name not present in dataset.")}
   if (!(y %in% names(data))){ stop("y column name not present in dataset.")}
   if (!(factor %in% names(data))){ stop("factor column name not present in dataset.")}
@@ -52,12 +55,19 @@ scatterHatch <- function(data, x, y, factor, legendTitle = "", pointSize = 1, po
       list(pattern = patterns[i], lineType = lineType, lineAlpha = lineAlpha, lineWidth = lineWidth, pointAlpha = pointAlpha)})
   }
 
-  if (length(patternList) != length(groups)){ # checks if patternList length is ok
-    stop("The length of patternList must be equal to the number of groups present.")
+  nOfPatterns = length(unique(vapply(patternList, function(i){i[[1]]}, character(1)))) # finds number of unique patterns
+  if (40 * nOfPatterns < length(groups)){
+    stop("Not enough unique combinations of patterns and columns for each group.")
   }
 
+
   if (is.null(colorPalette)){
-    colorPalette = dittoSeq::dittoColors(reps = ceiling(length(groups)/40))[1:length(groups)]
+    dittoColors = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#666666", "#AD7700", "#1C91D4",
+    "#007756", "#D5C711", "#005685", "#A04700", "#B14380", "#4D4D4D", "#FFBE2D", "#80C7EF", "#00F6B3", "#F4EB71",
+    "#06A5FF", "#FF8320", "#D99BBD", "#8C8C8C", "#FFCB57", "#9AD2F2", "#2CFFC6", "#F6EF8E", "#38B7FF", "#FF9B4D",
+    "#E0AFCA", "#A3A3A3", "#8A5F00", "#1674A9", "#005F45", "#AA9F0D", "#00446B", "#803800", "#8D3666", "#3D3D3D")
+    colorPalette = rep(dittoColors, times=ceiling(length(groups)/40))[1:length(groups)]
+    #colorPalette = dittoSeq::dittoColors(reps = ceiling(length(groups)/40))[1:length(groups)]
   }
 
   if (length(unique(colorPalette)) < length(groups)){
