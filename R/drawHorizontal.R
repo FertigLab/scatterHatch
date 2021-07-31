6282a3583961d53d4c50a3b685cddb25248b9bd5
roofTiles <guhatejas@gmail.com>
2021-07-31 09:25:33 -0400

    reduced number of lines over 80 characters
    from 18% of lines to 6% of lines below 80 chars

--- R/drawHorizontal.R
+++ R/drawHorizontal.R
@@ -3,78 +3,90 @@
 
 #' Draws the horizontal line pattern
 #'
-#' Using the 2D frequency matrix from countGridPoints(), this function goes row by row
-#' checking if each grid contains a point.  The function records the leftmost and rightmost points in a
-#' consecutive sequence of grids with points.  The function also records points in a sparse grid: defined
-#' as being a grid where grids n-rows or n-columns away do not contain any points.
+#' Using the 2D frequency matrix from countGridPoints(), this function 
+#' goes by row checking if each grid contains a point.  The function records 
+#' the leftmost and rightmost points in a consecutive sequence of grids with 
+#' points.  The function also records points in a sparse grid: defined
+#' as being a grid where grids n-rows or n-columns away do not contain any 
+#' points.
 #'
-#' @param gridOutput List: x & y bins of the grid, 2D frequency matrix, and dataframe matching each point to its corresponding grid in the matrix
-#' @param density Density of the pattern to be drawn.  Must be 1 or less.  Skips drawing the pattern every n rows.
+#' @param gridOutput List: x & y bins of the grid, 2D frequency matrix, and 
+#' dataframe matching each point to its corresponding grid in the matrix
+#' @param density Density of the pattern to be drawn.  Must be 1 or less.  
+#' Skips drawing the pattern every n rows.
 #' @param pointSize Point size of the pattern.
 #' @param xDiff x-coordinate range of the plot.
 #' @param yDiff y-coordinate range of the plot.
 #' @param rotatedxDiff Rotated x-coordinate range of the plot.
 #' @param rotatedyDiff Rotated y-coordinate range of the plot.
-#' @param sparsePoints Logical Vector denoting points annotated as sparse.  If NULL, default sparsity detector will be used to annotate sparse points in dataset.
-#' @return Dataframe with 4 columns defining the starting and ending coordinates of each line to be drawn.
+#' @param sparsePoints Logical Vector denoting points annotated as sparse.  
+#' If NULL, default sparsity detector will be used.
+#' @return Dataframe defining the coordinates of each line to be drawn.
 #' @noRd
 
-drawHorizontal <- function(gridOutput, density = NULL, pointSize, xDiff, yDiff, rotatedxDiff, rotatedyDiff, sparsePoints = NULL){
-  if (density > 1){density=1} # density must be 1 or less
-  #density = 1;
+drawHorizontal <- function(gridOutput, density = NULL, pointSize, xDiff, yDiff, 
+                           rotatedxDiff, rotatedyDiff, sparsePoints = NULL){
+  if (density > 1){density=1} ## density must be 1 or less
   xBins = gridOutput[[1]]
   yBins = gridOutput[[2]]
   freqMat = gridOutput[[3]]
   pointsToGrid = gridOutput[[4]]
-  sparsityAnnotateOutput = sparsityAnnotate(pointsToGrid, pointSize, rotatedxDiff, rotatedyDiff, 'x') # annotates sparse points and small clusters
   
-  if (is.null(sparsePoints)){ # if sparse points are not given
+  ## annotates sparse points and small clusters
+  sparsityAnnotateOutput = sparsityAnnotate(pointsToGrid, pointSize, 
+                                            rotatedxDiff, rotatedyDiff, 'x') 
+  if (is.null(sparsePoints)){ ## if sparse points are not given
     sparsePoints = sparsityAnnotateOutput$sparsePoints
   }
   
   sparsePointsToGrid = pointsToGrid[sparsePoints, ]
   smallClusterToGrid = pointsToGrid[sparsityAnnotateOutput$smallClusters, ]
   
-  pointsToGrid = pointsToGrid[!sparsePoints & !sparsityAnnotateOutput$smallClusters, ] # removes sparse and small cluster points from regular pattern drawing
+  ## removes sparse and small cluster points from regular pattern drawing
+  pointsToGrid = pointsToGrid[!sparsePoints & 
+                                !sparsityAnnotateOutput$smallClusters, ]
   
+  ## removes sparse points from 2D frequency matrix
   allIrregularPoints = rbind(sparsePointsToGrid, smallClusterToGrid)
-  for (i in seq(nrow(allIrregularPoints))){ # removes sparse points from 2D frequency matrix
-    freqMat[allIrregularPoints$yIntervals[i], allIrregularPoints$xIntervals[i]] = freqMat[allIrregularPoints$yIntervals[i], allIrregularPoints$xIntervals[i]] - 1
+  for (i in seq(nrow(allIrregularPoints))){ 
+    freqMat[allIrregularPoints$yIntervals[i], allIrregularPoints$xIntervals[i]] = 
+      freqMat[allIrregularPoints$yIntervals[i], allIrregularPoints$xIntervals[i]] - 1
   }
-  
+
   xStart = c()
   yStart = c()
   xEnd = c()
   yEnd = c()
   
-  rowDraw = TRUE # whether to draw lines in current row or not
-  for (row in seq(nrow(freqMat))){ # iterates by every row
+  rowDraw = TRUE ## whether to draw lines in current row or not
+  for (row in seq(nrow(freqMat))){ ## iterates by every row
     rowPoints = pointsToGrid[pointsToGrid$yIntervals == row, ]
     
-    #yLevels = yBins[row] - (yBins[1] - yBins[2])/2 # where to draw y level @Tejas's version
-    yLevels = yBins[row] - diff(yBins)[1]/2 #atul's version
-    if (row == nrow(freqMat)){ # for bottom row exception
+    ##yLevels = yBins[row] - (yBins[1] - yBins[2])/2 # where to draw y level
+    yLevels = yBins[row] - diff(yBins)[1]/2 ##atul's version
+    if (row == nrow(freqMat)){ ## for bottom row exception
       yLevels = yBins[row] - diff(yBins)[1]/2
     }
     
-    rowDraw = (as.integer(row * density)*(1/density)) == row # when to skip rows
+    rowDraw = (as.integer(row * density)*(1/density)) == row ## when to skip rows
     
     prevCol = 0
-    lineDraw = FALSE # whether line being drawn or not
+    lineDraw = FALSE ## whether line being drawn or not
     
     
     for (col in seq(ncol(freqMat))){
-      if (prevCol == 0 & freqMat[row, col] != 0  & rowDraw){ # starting a line segment
-        gridPoints = rowPoints[rowPoints$xIntervals == col, ] # points corresponding to current grid square
-        xStart = c(xStart, min(gridPoints$x)) # where to draw horizontal lines
+      ## starting a line segment
+      if (prevCol == 0 & freqMat[row, col] != 0  & rowDraw){ 
+        gridPoints = rowPoints[rowPoints$xIntervals == col, ]
+        xStart = c(xStart, min(gridPoints$x)) ## where to draw horizontal lines
         yStart = c(yStart, yLevels)
         lineDraw = TRUE
       }
       
-      if (lineDraw & freqMat[row, col] == 0 & rowDraw){ # ending line segment
-        gridPoints = rowPoints[rowPoints$xIntervals == col-1, ] # points corresponding to current grid square
-        
-        xEnd = c(xEnd, max(gridPoints$x)) # where to draw horizontal lines
+      ## ending line segment
+      if (lineDraw & freqMat[row, col] == 0 & rowDraw){ 
+        gridPoints = rowPoints[rowPoints$xIntervals == col-1, ]
+        xEnd = c(xEnd, max(gridPoints$x)) ## where to draw horizontal lines
         yEnd = c(yEnd, yLevels)
         lineDraw = FALSE
         
@@ -86,13 +98,13 @@ drawHorizontal <- function(gridOutput, density = NULL, pointSize, xDiff, yDiff, 
   }
   
   
-  # dealing with sparse points
+  ## dealing with sparse points
   xStart = c(xStart, sparsePointsToGrid$x)
   xEnd = c(xEnd, sparsePointsToGrid$x)
   yStart = c(yStart, sparsePointsToGrid$y)
   yEnd = c(yEnd, sparsePointsToGrid$y)
   
-  # dealing with small cluster points
+  ## dealing with small cluster points
   pointRadius = convertSizeToCartesian(pointSize, xDiff, 'x')
   smallClusterGridSize = as.integer(diff(rotatedxDiff)/(pointRadius * 5))
   smallClusterToGrid = countGridPoints(smallClusterToGrid$x, smallClusterToGrid$y, rotatedxDiff, rotatedyDiff, smallClusterGridSize)[[4]]
--- R/patternHelperFunctions.R
+++ R/patternHelperFunctions.R
@@ -14,12 +14,17 @@
 #' @param xDiff Range of the x-axis in the scatterplot
 #' @param yDiff Range of the y-axis in the scatterplot
 #' @param n Number of grids across each side of the square matrix
-#' @return List: x & y bins of the grid, 2D frequency matrix, and dataframe matching each point to its corresponding grid in the matrix
+#' @return List: x & y bins of the grid, 2D frequency matrix, and 
+#' dataframe matching each point to its corresponding grid in the matrix
 #' @noRd
 countGridPoints <- function(x, y, xDiff, yDiff, n){
-  xIntervals = as.numeric(cut(x, breaks = seq(xDiff[1], xDiff[2], by=diff(xDiff)/(n-1))))
-  yIntervals = as.numeric(cut(y, breaks = seq(yDiff[2], yDiff[1], by=-1*diff(yDiff)/(n-1))))
-  yIntervals = n+1-yIntervals # ensures as row index increases, y decreases in matrix
+  xIntervals = as.numeric(cut(x, breaks = seq(xDiff[1], xDiff[2], 
+                                              by=diff(xDiff)/(n-1))))
+  yIntervals = as.numeric(cut(y, breaks = seq(yDiff[2], yDiff[1], 
+                                              by=-1*diff(yDiff)/(n-1))))
+  
+  ## ensures as row index increases, y decreases in matrix
+  yIntervals = n+1-yIntervals
   intervals = as.data.frame(cbind(xIntervals, yIntervals))
   pointsToGrid = as.data.frame(cbind(xIntervals, yIntervals, x, y))
   freqs = plyr::count(intervals, vars=c("xIntervals", "yIntervals"))
@@ -46,24 +51,28 @@ countGridPoints <- function(x, y, xDiff, yDiff, n){
 #' @return Radius of the point in Cartesian units
 #' @noRd
 convertSizeToCartesian <- function(size, scale, axis){
-  #fontSize = size*ggplot2::.pt + ggplot2::.stroke*0.5/2
+  ##fontSize = size*ggplot2::.pt + ggplot2::.stroke*0.5/2
   fontSize = size*ggplot2::.pt
   aspectRatio = dev.size()[1]/dev.size()[2]
   if (aspectRatio >= 1){
     if (axis == 'x'){
-      cartesianConvert = grid::convertWidth(grid::unit(fontSize, "points"), unitTo="npc", valueOnly = TRUE) * diff(scale)/aspectRatio
+      cartesianConvert = grid::convertWidth(grid::unit(fontSize, "points"), 
+                                            unitTo="npc", valueOnly = TRUE) * diff(scale)/aspectRatio
     }
     if (axis == 'y'){
-      cartesianConvert = grid::convertHeight(grid::unit(fontSize, "points"), unitTo="npc", valueOnly = TRUE) * diff(scale)/aspectRatio
+      cartesianConvert = grid::convertHeight(grid::unit(fontSize, "points"), 
+                                             unitTo="npc", valueOnly = TRUE) * diff(scale)/aspectRatio
     }
   }
   
   if (aspectRatio < 1){
     if (axis == 'x'){
-      cartesianConvert = grid::convertWidth(grid::unit(fontSize, "points"), unitTo="npc", valueOnly = TRUE) * diff(scale)*aspectRatio
+      cartesianConvert = grid::convertWidth(grid::unit(fontSize, "points"), 
+                                            unitTo="npc", valueOnly = TRUE) * diff(scale)*aspectRatio
     }
     if (axis == 'y'){
-      cartesianConvert = grid::convertHeight(grid::unit(fontSize, "points"), unitTo="npc", valueOnly = TRUE) * diff(scale)*aspectRatio
+      cartesianConvert = grid::convertHeight(grid::unit(fontSize, "points"), 
+                                             unitTo="npc", valueOnly = TRUE) * diff(scale)*aspectRatio
     }
   }
   
