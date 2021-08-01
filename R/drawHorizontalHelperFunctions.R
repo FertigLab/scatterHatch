935e2269b6c0eb8e9125686adbb06e4abd079034
roofTiles <guhatejas@gmail.com>
2021-08-01 17:57:24 -0400

    tidying horizontal helper functions
    Former-commit-id: e0d2db42db6d11516efe9395eb3ccfb84faaa3fa

--- R/drawHorizontalHelperFunctions.R
+++ R/drawHorizontalHelperFunctions.R
@@ -10,30 +10,30 @@
 getIrregularPoints <- function(pointsToGrid, freqMat, sparsePoints, 
                                rotatedxRange, rotatedyRange, xRange, pointSize){
   
-  sparsityAnnotateOutput <- sparsityAnnotate(pointsToGrid, pointSize, 
+    sparsityAnnotateOutput <- sparsityAnnotate(pointsToGrid, pointSize, 
                                             rotatedxRange, rotatedyRange, 'x')
   
-  ## if sparse points are not given
-  if (is.null(sparsePoints)){ 
-    sparsePoints <- sparsityAnnotateOutput$sparsePoints
-  }
+    ## if sparse points are not given
+    if (is.null(sparsePoints)){ 
+      sparsePoints <- sparsityAnnotateOutput$sparsePoints
+    }
   
-  sparsePointsToGrid <- pointsToGrid[sparsePoints, ]
-  smallClusterToGrid <- pointsToGrid[sparsityAnnotateOutput$smallClusters, ]
+    sparsePointsToGrid <- pointsToGrid[sparsePoints, ]
+    smallClusterToGrid <- pointsToGrid[sparsityAnnotateOutput$smallClusters, ]
   
-  ## removes sparse and small cluster points from regular pattern drawing
-  pointsToGrid <- pointsToGrid[!sparsePoints & 
+    ## removes sparse and small cluster points from regular pattern drawing
+    pointsToGrid <- pointsToGrid[!sparsePoints & 
                                 !sparsityAnnotateOutput$smallClusters, ]
   
-  ## removes sparse points from 2D frequency matrix
-  allIrregularPoints <- rbind(sparsePointsToGrid, smallClusterToGrid)
-  for (i in seq(nrow(allIrregularPoints))){ 
-    freqMat[allIrregularPoints$yIntervals[i], allIrregularPoints$xIntervals[i]]=
+    ## removes sparse points from 2D frequency matrix
+    allIrregularPoints <- rbind(sparsePointsToGrid, smallClusterToGrid)
+    for (i in seq(nrow(allIrregularPoints))){ 
       freqMat[allIrregularPoints$yIntervals[i], 
-              allIrregularPoints$xIntervals[i]] - 1
-  }
+              allIrregularPoints$xIntervals[i]]= freqMat[allIrregularPoints$yIntervals[i], 
+                                                         allIrregularPoints$xIntervals[i]] - 1
+      }
   
-  return(list(sparsePointsToGrid, smallClusterToGrid, pointsToGrid, freqMat))
+    return(list(sparsePointsToGrid, smallClusterToGrid, pointsToGrid, freqMat))
 }
 
 #' Finds the line segments for drawing patterns in large clusters
@@ -43,42 +43,42 @@ getIrregularPoints <- function(pointsToGrid, freqMat, sparsePoints,
 #' @param density Density of pattern drawing (how many rows to skip)
 #' @noRd
 regularPatternDraw <- function(freqMat, pointsToGrid, yBins, density){
-  xStart <- yStart <- xEnd <- yEnd <- c()
-  rowDraw <- TRUE # whether to draw lines in current row or not
+    xStart <- yStart <- xEnd <- yEnd <- c()
+    rowDraw <- TRUE # whether to draw lines in current row or not
   
-  for (row in seq(nrow(freqMat))){ # iterates by every row
-    rowPoints <- pointsToGrid[pointsToGrid$yIntervals == row, ]
+    for (row in seq(nrow(freqMat))){ # iterates by every row
+      rowPoints <- pointsToGrid[pointsToGrid$yIntervals == row, ]
     
-    yLevels <- yBins[row] - diff(yBins)[1]/2 # atul's version
-    if (row == nrow(freqMat)){ # for bottom row exception
-      yLevels <- yBins[row] - diff(yBins)[1]/2
-    }
-    
-    rowDraw <- (as.integer(row * density)*(1/density)) == row #when to skip rows
-    
-    prevCol <- 0
-    lineDraw <- FALSE # whether line being drawn or not
-    
-    for (col in seq(ncol(freqMat))){
-      ## starting a line segment
-      if (prevCol == 0 & freqMat[row, col] != 0  & rowDraw){ 
-        gridPoints <- rowPoints[rowPoints$xIntervals == col, ]
-        xStart <- c(xStart, min(gridPoints$x)) # where to draw horizontal lines
-        yStart <- c(yStart, yLevels)
-        lineDraw <- TRUE
+      yLevels <- yBins[row] - diff(yBins)[1]/2 # atul's version
+      if (row == nrow(freqMat)){ # for bottom row exception
+        yLevels <- yBins[row] - diff(yBins)[1]/2
       }
+    
+      rowDraw <- (as.integer(row * density)*(1/density)) == row #when to skip
+    
+      prevCol <- 0
+      lineDraw <- FALSE # whether line being drawn or not
+    
+      for (col in seq(ncol(freqMat))){
+        ## starting a line segment
+        if (prevCol == 0 & freqMat[row, col] != 0  & rowDraw){ 
+          gridPoints <- rowPoints[rowPoints$xIntervals == col, ]
+          xStart <- c(xStart, min(gridPoints$x)) # where to draw horizontal line
+          yStart <- c(yStart, yLevels)
+          lineDraw <- TRUE
+        }
       
-      ## ending line segment
-      if (lineDraw & freqMat[row, col] == 0 & rowDraw){ 
-        gridPoints <- rowPoints[rowPoints$xIntervals == col-1, ]
-        xEnd <- c(xEnd, max(gridPoints$x)) # where to draw horizontal lines
-        yEnd <- c(yEnd, yLevels)
-        lineDraw <- FALSE
+        ## ending line segment
+        if (lineDraw & freqMat[row, col] == 0 & rowDraw){ 
+          gridPoints <- rowPoints[rowPoints$xIntervals == col-1, ]
+          xEnd <- c(xEnd, max(gridPoints$x)) # where to draw horizontal lines
+          yEnd <- c(yEnd, yLevels)
+          lineDraw <- FALSE
+        }
+      
+        prevCol <- freqMat[row, col]
       }
-      
-      prevCol <- freqMat[row, col]
     }
+  
+    return(list(xStart, xEnd, yStart, yEnd))
   }
-  
-  return(list(xStart, xEnd, yStart, yEnd))
-}
