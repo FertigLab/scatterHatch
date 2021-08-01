29ca249ac310dc873624cebd2478d89fef24dfdc
roofTiles <guhatejas@gmail.com>
2021-08-01 17:54:26 -0400

    tidying legendHelperFunctions
    Former-commit-id: ad76ef4840c90b2aba41aa03fdb1e3b64c9199da

--- R/legendHelperFunctions.R
+++ R/legendHelperFunctions.R
@@ -17,49 +17,48 @@
 #' @return List of grob objects
 #' @noRd
 legendIcon <- function(color, lineColor="black", lineType="solid", 
-                       patternType, lineWidth=1.5, angle=NULL){
-  radius <- 0.25
-  linePar <- grid::gpar(col = lineColor, fill = color, lwd = 2*lineWidth, 
-                       lty = lineType, alpha=1)
-  circle <- grid::circleGrob(0.5, 0.5, radius, gp=grid::gpar(
-            col=color, fill=color, lwd=1, alpha=1))
-  grobs <- list(circle)
-  grobs[["gp"]] <- grid::gpar(alpha = 1)
-
-  if (patternType %in% c("blank", "")){
-    return(do.call(grid::grobTree, args=grobs))
-  }
-
-  angle <- -angle
-  angle <- (angle/180) * pi ## converting angle from radians to degrees
-  phaseDiff <- pi/6
-
-  for (a in angle){
-    ## x/y coordinates of center line segment
-    centralLineX <- c(radius * cos(a) + 0.5, -radius * cos(a) + 0.5) 
-    centralLineY <- c(radius * sin(a) + 0.5, -radius * sin(a) + 0.5)
-    grobs[[length(grobs) + 1]] <- grid::linesGrob(x=centralLineX, 
-                                                 y=centralLineY, gp=linePar)
+                       patternType, lineWidth=1.5, angle=NULL) {
+    radius <- 0.25
+    linePar <- grid::gpar(col = lineColor, fill = color, lwd = 2 * lineWidth, 
+                          lty = lineType, alpha = 1)
+    circle <- grid::circleGrob(0.5, 0.5, radius, gp = grid::gpar(col = color, 
+                                            fill = color, lwd = 1, alpha = 1))
+    grobs <- list(circle)
+    grobs[["gp"]] <- grid::gpar(alpha = 1)
+  
+    if (patternType %in% c("blank", "")) {
+      return(do.call(grid::grobTree, args = grobs))
+    }
+  
+    angle <- -angle
+    angle <- (angle/180) * pi  ## converting angle from radians to degrees
+    phaseDiff <- pi/6
+  
+    for (a in angle) {
+        ## x/y coordinates of center line segment
+        centralLineX <- c(radius * cos(a) + 0.5, -radius * cos(a) + 0.5)
+        centralLineY <- c(radius * sin(a) + 0.5, -radius * sin(a) + 0.5)
+        grobs[[length(grobs) + 1]] <- grid::linesGrob(x = centralLineX, 
+                                                 y = centralLineY, gp = linePar)
     
-    ## x/y coordinates of left line segment
-    leftLineX <- c(radius * cos(a + phaseDiff) + 0.5, -radius * 
-                    cos(a - phaseDiff) + 0.5)
-    leftLineY <- c(radius * sin(a + phaseDiff) + 0.5, -radius * 
-                    sin(a - phaseDiff) + 0.5)
-    grobs[[length(grobs) + 1]] <- grid::linesGrob(x=leftLineX, 
-                                                 y=leftLineY, gp=linePar)
+        ## x/y coordinates of left line segment
+        leftLineX <- c(radius * cos(a + phaseDiff) + 0.5, -radius * 
+                       cos(a - phaseDiff) + 0.5)
+        leftLineY <- c(radius * sin(a + phaseDiff) + 0.5, -radius * 
+                       sin(a - phaseDiff) + 0.5)
+        grobs[[length(grobs) + 1]] <- grid::linesGrob(x = leftLineX, 
+                                                    y = leftLineY, gp = linePar)
     
-    ## x/y coordinates of right line segment
-    rightLineX <- c(radius * cos(a - phaseDiff) + 0.5, -radius * 
-                     cos(a + phaseDiff) + 0.5) 
-    rightLineY <- c(radius * sin(a - phaseDiff) + 0.5, -radius * 
-                     sin(a + phaseDiff) + 0.5)
-    grobs[[length(grobs) + 1]] <- grid::linesGrob(x=rightLineX, 
-                                                 y=rightLineY, gp=linePar)
-  }
-
-  return(do.call(grid::grobTree, args=grobs))
-
+        ## x/y coordinates of right line segment
+        rightLineX <- c(radius * cos(a - phaseDiff) + 0.5, -radius * 
+                          cos(a + phaseDiff) + 0.5)
+        rightLineY <- c(radius * sin(a - phaseDiff) + 0.5, -radius * 
+                          sin(a + phaseDiff) + 0.5)
+        grobs[[length(grobs) + 1]] <- grid::linesGrob(x = rightLineX, 
+                                                   y = rightLineY, gp = linePar)
+    }
+  
+    return(do.call(grid::grobTree, args = grobs))
 }
 
 #' Creates custom ggplot2 object
@@ -104,9 +103,9 @@ imagePoints <- ggplot2::ggproto("imagePoints", ggplot2::Geom,
 geom_imagePoint <- function(mapping=NULL, data=NULL, stat="identity",
                             position="identity", na.rm=FALSE, 
                             show.legend=NA, inherit.aes=TRUE, ...) {
-  ggplot2::layer(
-    geom = imagePoints, mapping=mapping,  data=data, stat=stat,
-    position=position, show.legend=show.legend, inherit.aes=inherit.aes,
-    params=list(na.rm = na.rm, ...)
-  )
+      ggplot2::layer(
+        geom = imagePoints, mapping=mapping,  data=data, stat=stat,
+        position=position, show.legend=show.legend, inherit.aes=inherit.aes,
+        params=list(na.rm = na.rm, ...)
+      )
 }
